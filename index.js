05d882938ca20ce780c4a63a4a81d7603ed7026e
Zolotov <admin@Zolotovs-MacBook-Pro.local>
2020-07-09 14:55:50 +0200

    Added code to handle cors errors under the middleware section

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Origin-Headers', '*');
 
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Origin', 'PUT', 'POST', 'POST', 'DELETE', 'PATCH');
-        return res.status(200).json({});
-    }
+    // if (req.method === 'OPTIONS') {
+    //     res.header('Access-Control-Allow-Origin', 'PUT', 'POST', 'POST', 'DELETE', 'PATCH');
+    //     return res.status(200).json({});
+    // }
 
     next();
 });
